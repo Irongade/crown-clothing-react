575b4bac8d7cd0f1252b00e43b812de355c78c20
Irongade <adeoluayangade@yahoo.com>
2020-07-21 00:12:48 +0100

    fixed import error bugs

--- src/redux/cart/cart.action.js
+++ src/redux/cart/cart.action.js
@@ -1,9 +1,9 @@
 import CartActionTypes from "./cart.types"
 
-const toggleCartHidden = () => ({
+
+export const toggleCartHidden = () => ({
     type: CartActionTypes.TOGGLE_CART_HIDDEN
 }   
 )
 
 
-export default toggleCartHidden;
